b6915f72a0e32120aeaa00b418cf3452f2d3663f
Pasha <pasha@2vega.com>
2019-02-12 22:09:58 -0800

    Initial Commit

---
+++ SwiftBeacon/SwiftBeacon.h
@@ -0,0 +1,19 @@
+//
+//  SwiftBeacon.h
+//  SwiftBeacon
+//
+//  Created by Pavel Skaldin on 2/12/19.
+//  Copyright © 2019 Pavel Skaldin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftBeacon.
+FOUNDATION_EXPORT double SwiftBeaconVersionNumber;
+
+//! Project version string for SwiftBeacon.
+FOUNDATION_EXPORT const unsigned char SwiftBeaconVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftBeacon/PublicHeader.h>
+
+
