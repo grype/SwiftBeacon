5a6c3e0b1c922b4c4a90925ab8f81ded6fcb4408
Pasha <pasha@2vega.com>
2019-12-27 14:30:20 -0800

    Separate dir for TestCases & update precommit checker
    precommit now checks for actual diffs before tripping

--- scripts/git-hooks/pre-commit/check-linuxmain.sh
+++ scripts/git-hooks/pre-commit/check-linuxmain.sh
@@ -2,23 +2,12 @@
 
 RED='\033[0;31m'
 CLEAR='\033[0m'
-MANIFEST="Tests/BeaconTests/XCTestManifests.swift"
 
-findNewerTests() {
-	find Tests/BeaconTests  -type f -iname "*.swift" -newer "${MANIFEST}"
+hasTestCaseChanges() {
+  ! git diff --quiet Tests/BeaconTests/Test\ Cases
 }
 
-isLinuxMainFresh() {
-  type -a $tests
-  tests=(`find Tests/BeaconTests  -type f -iname "*.swift" -newer "${MANIFEST}"`)
-	if [[ ${#tests[@]} -eq 0 ]]; then
-    return 0
-  else
-    return 1
-  fi
-}
-
-if ! isLinuxMainFresh; then
+if hasTestCaseChanges; then
 	echo "${RED}Test files changed!${CLEAR}"
 	echo "Update LinuxMain.swift before committing:"
 	echo "  make linuxmain"
