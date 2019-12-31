3cf823763c04bc5b7c15455ead4628e5cc8ccd8f
Pasha <pasha@2vega.com>
2019-12-30 17:00:51 -0800

    Remove echoes in pre-commit hook
    That trips Xcode

--- scripts/git-hooks/pre-commit.sh
+++ scripts/git-hooks/pre-commit.sh
@@ -2,13 +2,7 @@
 
 GIT_DIR=$(git rev-parse --show-toplevel)
 SCRIPTS_DIR="${GIT_DIR}/scripts/git-hooks/pre-commit"
-alias -g ERR='>&2'
-
-echo ERR
-echo "Running pre-commit hooks..." ERR
 
 for i in $SCRIPTS_DIR/**/*(.); do
-  echo ERR
-  echo "> ${i:t}" ERR
   $i || return 1
 done
