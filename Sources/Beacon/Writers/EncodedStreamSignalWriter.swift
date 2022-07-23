ed023c124930c4b6cffbaed3b432bff6a3a151c6
Pasha <pasha@2vega.com>
2022-07-22 17:48:51 -0700

    Default to new line for separating streamed log entries

--- Sources/Beacon/Writers/EncodedStreamSignalWriter.swift
+++ Sources/Beacon/Writers/EncodedStreamSignalWriter.swift
@@ -1,6 +1,6 @@
 //
 //  EncodedStreamSignalWriter.swift
-//  
+//
 //
 //  Created by Pavel Skaldin on 8/26/21.
 //  Copyright © 2021 Pavel Skaldin. All rights reserved.
@@ -10,24 +10,25 @@ import Foundation
 
 /**
  I encode `Signals` onto an `OutputStream`
+ 
+ I separate each entry using a `separator`, which defaults to new line character.
  */
 
-public class EncodedStreamSignalWriter : SignalWriter {
-    
+public class EncodedStreamSignalWriter: SignalWriter {
     open var encoder: SignalEncoder
     
     open var stream: OutputStream
     
-    open var separator: Data = .init()
+    open var separator: Data = "\n".data(using: .utf8)!
     
-    // MARK:- Init
+    // MARK: - Init
     
     public init(on aStream: OutputStream, encoder anEncoder: SignalEncoder) {
         stream = aStream
         encoder = anEncoder
     }
     
-    // MARK:- Opening/Closing
+    // MARK: - Opening/Closing
     
     open func open() {
         stream.open()
@@ -37,13 +38,11 @@ public class EncodedStreamSignalWriter : SignalWriter {
         stream.close()
     }
     
-    
-    // MARK:- Writing
+    // MARK: - Writing
     
     open func write(_ aSignal: Signal) throws {
         let data = try encoder.encode(aSignal)
         data.write(on: stream)
         separator.write(on: stream)
     }
-    
 }
