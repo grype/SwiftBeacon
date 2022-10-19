723cbbe2dec745ad81a5551eb69c48ad77b64c0d
Pasha <pasha@2vega.com>
2022-10-18 21:50:12 -0700

    Simplify MakeSignal snippet

--- Xcode/Snippets/MakeSignal.swift
+++ Xcode/Snippets/MakeSignal.swift
@@ -1,22 +1,10 @@
-class <#Signal#> : WrapperSignal {
-    typealias T = <#WrappedType#>
-    
-    var <#accessor#>: T {
-        return value as! T
-    }
-    
-    override var signalName: String {
-        return "<#identifier#>"
-    }
-    
+typealias T = <#WrappedType#>
+
+public class <#Signal#>: WrapperSignal {
+    var <#valueAccessor#>: T { value as! T }
+    override public var signalName: String { "<#identifier#>" }
 }
 
-extension <#WrappedType#> : Signaling {
-    public var beaconSignal: Signal {
-        return <#Signal#>(self)
-    }
-}
-
-func emit(_ value: <#WrappedType#>, on beacon: Beacon = Beacon.shared, userInfo: [AnyHashable : Any]? = nil, fileName: String = #file, line: Int = #line, functionName: String = #function) {
-    <#Signal#>(value).emit(on: [beacon], userInfo: userInfo, fileName: fileName, line: line, functionName: functionName)
+extension T: Signaling {
+    public var beaconSignal: <#Signal#> { .init(self) }
 }
