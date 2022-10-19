02c43882cf14a5f60e5385959f3a10a606943e09
Pasha <pasha@2vega.com>
2022-10-19 15:40:01 -0700

    Add playground page with examples of filtering

---
+++ Playground.playground/Pages/Filtering.xcplaygroundpage/Contents.swift
@@ -0,0 +1,32 @@
+//: [Previous](@previous)
+
+import Beacon
+import Foundation
+import PlaygroundSupport
+
+extension String: Error {}
+
+let console = ConsoleLogger(name: "Console")
+
+func doIt() {
+    console.run { _ in
+        if willLog(type: StringSignal.self, on: [.shared]) {
+            // will log a string signal only if there's a running logger listening on .shared beacon
+            // and there aren't any constraints prohibiting it
+            emit("Logging string signal")
+        }
+        else {
+            // otherwise, this will get logged, and that's because
+            // we're not filtering out ErrorSignal's anywhere
+            emit(error: "WARNING: String signals are disabled!")
+        }
+    }
+}
+
+StringSignal.disable(loggingTo: console, on: nil)
+doIt()
+
+StringSignal.enable(loggingTo: console, on: nil)
+doIt()
+
+//: [Next](@next)
