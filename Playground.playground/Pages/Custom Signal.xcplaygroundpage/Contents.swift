2eb17d42b7dd11779e049a46164602b0ce5cf25a
Pasha <pasha@2vega.com>
2022-10-18 21:39:46 -0700

    Clean up Playground examples

---
+++ Playground.playground/Pages/Custom Signal.xcplaygroundpage/Contents.swift
@@ -0,0 +1,28 @@
+//: [Previous](@previous)
+
+import Beacon
+import Foundation
+import PlaygroundSupport
+
+typealias T = PlaygroundPage
+
+public class PlaygroundPageSignal: WrapperSignal {
+    var page: T { value as! T }
+    override public var signalName: String { "📃" }
+    override public var valueDescription: String? {
+        "\(super.valueDescription ?? "") needsInfiniteExecution: \(page.needsIndefiniteExecution)"
+    }
+}
+
+extension T: Signaling {
+    public var beaconSignal: PlaygroundPageSignal {
+        return PlaygroundPageSignal(self)
+    }
+}
+
+let logger = ConsoleLogger(name: "Playground Console")
+logger.run { _ in
+    emit(PlaygroundSupport.PlaygroundPage.current)
+}
+
+//: [Next](@next)
--- Playground.playground/Pages/Scratchpad.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Scratchpad.xcplaygroundpage/Contents.swift
@@ -1,11 +1,18 @@
 //: [Previous](@previous)
 
-import Foundation
 import Beacon
+import Foundation
+import PlaygroundSupport
 
-let logger = ConsoleLogger(name: "Playground")
+extension String: Error {}
+
+let logger = ConsoleLogger(name: "Playground Console")
 logger.run { _ in
     emit()
+    emit("A Message")
+    emit(Date())
+    emit(URL(string: "http://example.com")!)
+    do { throw "🦝" } catch { emit(error: error) }
 }
 
 //: [Next](@next)
