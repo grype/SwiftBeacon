c8d53184b99aa73d4cad7fb2a8407f179e5667ee
Pasha <pasha@2vega.com>
2022-10-29 22:56:07 -0700

    Update playground

--- Playground.playground/Pages/Filtering.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Filtering.xcplaygroundpage/Contents.swift
@@ -14,8 +14,7 @@ func doIt() {
             // will log a string signal only if there's a running logger listening on .shared beacon
             // and there aren't any constraints prohibiting it
             emit("Logging string signal")
-        }
-        else {
+        } else {
             // otherwise, this will get logged, and that's because
             // we're not filtering out ErrorSignal's anywhere
             emit(error: "WARNING: String signals are disabled!")
