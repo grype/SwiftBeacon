de88d6ae2aa8cfa434fc038fdc628e077ae5cf11
Pasha <pasha@2vega.com>
2024-05-09 17:27:41 -0700

    Emit published values onto a subject

--- Sources/Beacon/Extensions/Combine+Emit.swift
+++ Sources/Beacon/Extensions/Combine+Emit.swift
@@ -11,24 +11,38 @@ import Foundation
 public extension Publisher
 {
     // Subscribes to the given `Subject`
-    func emit<S: Subscriber>(userInfo: Any? = nil, source: Source = .init(), on subject: S)
+    func emit<S: Subscriber>(userInfo: Any? = nil, source: Source = .init(), on subscriber: S)
         where S.Input == Signal,
         S.Failure == Never,
         Failure == S.Failure,
         Output == S.Input
     {
-        subscribe(subject)
+        subscribe(subscriber)
     }
 
     // Maps any value to an appropriate type of `Signal` and subscribes to the resulting `Publisher`.
     // The type of signal is derived using `Signal.representing()`.
-    func emit<S: Subscriber, V: Any>(userInfo: Any? = nil, source: Source = .init(), on subject: S)
+    func emit<S: Subscriber, V: Any>(userInfo: Any? = nil, source: Source = .init(), on subscriber: S)
         where S.Input == Signal,
         S.Failure == Never,
         Failure == S.Failure,
         Output == V
     {
         map { Signal.representing($0, userInfo: userInfo, source: source) }
-            .subscribe(subject)
+            .subscribe(subscriber)
+    }
+
+    func emit<S: Subject, V: Any>(userInfo: Any? = nil, source: Source = .init(), on subject: S) -> AnyCancellable
+        where S.Output == Signal,
+        S.Failure == Never,
+        Failure == S.Failure,
+        Output == V
+    {
+        map { Signal.representing($0, userInfo: userInfo, source: source) }.sink
+        { aCompletion in
+            subject.send(completion: aCompletion)
+        } receiveValue: { anOutput in
+            subject.send(anOutput)
+        }
     }
 }
